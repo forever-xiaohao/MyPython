7e56bcd6c55f58d6c5d177e28751be3e4d46ab89
forever-xiaohao <1012958103@qq.com>
2018-10-11 23:33:07 +0800

    Initial commit

---
+++ com/csic/__init__.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python
+#-*- coding:utf-8 _*-
+"""
+@author:haoc
+@file: __init__.py.py
+@time: 2018/10/{DAY}
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
